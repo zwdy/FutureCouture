35df4b21dc30476d84b57a1a375f81baa0c9d703
pknowledge <zwendyyd@gmail.com>
2022-08-14 22:37:31 -0700

    second commit

--- scriptLookbook.js
+++ scriptLookbook.js
@@ -1,3 +1,143 @@
+function switchText01() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Tie Pants</span><br>Tie: 59.00%<br>Indigo/Beige<br><span style='background-color:black;color:white;'>230.00 USD</span>";
+  }
+}
+
+function switchText02() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Kite Dress</span><br>Kite: 72.06%<br>Olive/Beige<br><span style='background-color:black;color:white;'>180.00 USD</span>";
+  }
+}
+
+function switchText03() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Horse Coat</span><br>Kite: 93.15%<br>Destressed Denim<br><span style='background-color:black;color:white;'>490.00 USD</span>";
+  }
+}
+
+function switchText04() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Skateboard Tee</span><br>Skateboard: 91.30%<br>Light Denim/Cream<br><span style='background-color:black;color:white;'>120.00 USD</span>";
+    }
+}
+
+function switchText05() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Vase Dress</span><br>Vase: 99.87%<br>Mauve<br><span style='background-color:black;color:white;'>650.00 USD</span>";
+    }
+}
+
+function switchText06() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Bird Cape</span><br>Bird: 96.30%<br>Baby Blue<br><span style='background-color:black;color:white;'>570.00 USD</span>";
+    }
+}
+
+function switchText07() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Fire Hydrant Jacket</span><br>Fire Hydrant: 60.00%<br>Taupe/Light Denim<br><span style='background-color:black;color:white;'>280.00 USD</span>";
+    }
+}
+
+function switchText09() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Scissor Blazer</span><br>Scissor: 98.89%<br>Yellow/Navy<br><span style='background-color:black;color:white;'>470.00 USD</span>";
+    }
+}
+
+function switchText10() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Suitcase Shirt</span><br>Suitcase: 98.10%<br>Grey/Navy/Blue<br><span style='background-color:black;color:white;'>380.00 USD</span>";
+    }
+}
+
+function switchText11() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Kite Long Coat</span><br>Kite: 58.92%<br>Gold/Khaki<br><span style='background-color:black;color:white;'>800.00 USD</span>";
+    }
+}
+
+function switchText12() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Dog Shirt</span><br>Dog: 99.77%<br>White Mesh<br><span style='background-color:black;color:white;'>500.00 USD</span>";
+    }
+}
+
+function switchText13() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Backpack Pants</span><br>Backpack: 86.22%<br>Mustard/Basil<br><span style='background-color:black;color:white;'>390.00 USD</span>";
+    }
+}
+
+function switchText14() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Cow Skirt</span><br>Cow: 82.19%<br>Sage/Black<br><span style='background-color:black;color:white;'>620.00 USD</span>";
+    }
+}
+
+function switchText15() {
+  var x = document.getElementById("itemDescription");
+  if (x.innerHTML === "hello!") {
+    x.innerHTML = "hello";
+  } else {
+    x.innerHTML =
+    "<span style='font-size:110%;'>Dog Cardigan</span><br>Cow: 92.82%<br>Blush/Black<br><span style='background-color:black;color:white;'>480.00 USD</span>";
+    }
+}
+
 
 
 
@@ -7,7 +147,6 @@ function show01() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/001.jpg"
-
     }
 
 function show02() {
@@ -15,7 +154,6 @@ function show02() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/002.jpg"
-
     }
 
 function show03() {
@@ -23,8 +161,6 @@ function show03() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/003.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show04() {
@@ -32,8 +168,6 @@ function show04() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/004.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show05() {
@@ -41,8 +175,6 @@ function show05() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/005.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show06() {
@@ -50,8 +182,6 @@ function show06() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/006.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show07() {
@@ -59,8 +189,6 @@ function show07() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/007.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show08() {
@@ -68,8 +196,6 @@ function show08() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/008.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
  function show09() {
@@ -77,8 +203,6 @@ function show08() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/009.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show10() {
@@ -86,8 +210,6 @@ function show10() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/010.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show11() {
@@ -95,8 +217,6 @@ function show11() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/011.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show12() {
@@ -104,8 +224,6 @@ function show12() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/012.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show13() {
@@ -113,8 +231,6 @@ function show13() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/013.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show14() {
@@ -122,8 +238,6 @@ function show14() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/014.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
 
 function show15() {
@@ -131,17 +245,4 @@ function show15() {
     let image = document.getElementById("image");
     image.src =
                 "image_lookbook_onclick/015.jpg"
-    document.getElementById("btnID")
-            .style.display = "none";
     }
-
-
- /* Template*/
-    /*function show03() {*/
-    /* Get image and change value of src attribute */
-    /*    let image = document.getElementById("image");
-        image.src =
-                    "image_lookbook_onclick/03.jpg"
-        document.getElementById("btnID03")
-                .style.display = "none";
-        }*/
